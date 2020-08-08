f71082e9b451d5ced592ccc383cb27758500eecb
Durjoy Dey <durjoydey001@gmail.com>
2020-08-08 19:34:31 +0600

    Update Count pairs with given sum.cpp

--- Arrays/GeeksForGeeks Solved Problem/Count pairs with given sum.cpp
+++ Arrays/GeeksForGeeks Solved Problem/Count pairs with given sum.cpp
@@ -1,5 +1,6 @@
 //https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum/0
 
+                                     /* Time complexity O(n^2)*/
 #include<bits/stdc++.h>
 using namespace std;
 int Total_pairs(int arr[],int n,int k)
@@ -34,3 +35,48 @@ int main()
         cout<<Z<<endl;
     }
 }
+
+                                 /* Better Solution**/
+                                 /* Time Complexity O(n)*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int Total_pairs(int arr[],int n,int k)
+{
+    int ans = 0;
+    map<int,int>m;
+    for(int i=0;i<n;i++)
+    {
+        m[arr[i]]++;
+    }
+    for(int i=0;i<n;i++)
+    {
+        ans = ans+m[k-arr[i]];
+        if(k-arr[i]==arr[i])
+        {
+            ans--;
+        }
+    }
+    return ans/2;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,Z;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Z = Total_pairs(arr,n,k);
+        cout<<Z<<endl;
+    }
+    /* 6 8
+       4 6 2 6 2 2 */
+}
+
+
