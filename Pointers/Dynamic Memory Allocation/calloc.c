b0256eac576da60b8277abd5f7d91c868b1ff115
Durjoy Dey <durjoydey001@gmail.com>
2020-08-19 00:29:42 +0600

    Create calloc.c

---
+++ Pointers/Dynamic Memory Allocation/calloc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    printf("Enter size of an array\n");
+    scanf("%d",&n);
+    int *A = (int*)calloc(n,sizeof(int));//dynamically allocated array
+    //calloc function return void pointer that's why we type cast it to pointer to integer.
+    //pointer A points the first address of heap memory.
+    for(int i=0;i<n;i++)
+    {
+        A[i] = i+1;
+    }
+    // in calloc all value of address initially set to 0
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",A[i]);
+    }
+    free(A);
+}
+
