0be08030a479f6e0afc901778b7a0a2486a769a7
Durjoy Dey <durjoydey001@gmail.com>
2020-08-22 00:18:50 +0600

    Update Inserting a node at beginning.cpp

--- Linked List/Singly Linked List/Inserting a node/Inserting a node at beginning.cpp
+++ Linked List/Singly Linked List/Inserting a node/Inserting a node at beginning.cpp
@@ -2,6 +2,7 @@ https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
 
 #include<bits/stdc++.h>
 using namespace std;
+// define node
 class Node{
 public:
     int data;
@@ -14,18 +15,18 @@ Node* head = NULL;//head node which point the first node,initially empty
  //make this global is easy to implement.
 insertNode(int X)
 {
-    Node* temp = new Node();
-    temp->data = X;
-    temp->next = head;
+    Node* temp = new Node();//allocate memory in heap
+    temp->data = X;//insert data
+    temp->next = head;//pointer to nodes,which point to next node.
     head = temp;//insert node in beginning that's why head address is changed
 }
 printList()
 {
-    Node* N = head;
+    Node* N = head;//copy head node address in variable N,because we don't want to manupulate the head node address.
     while(N != NULL)
     {
-        cout<<N->data<<" ";
-        N = N->next;
+        cout<<N->data<<" ";//print data
+        N = N->next;//current node data already printed ,and this current node also contain next node address,so we jsut copy next node address in N. 
     }
     cout<<endl;
 }
@@ -40,6 +41,6 @@ int main()
         int X;
         cin>>X;
         insertNode(X);
-        printList();
+        printList();//print list every time after data insert
     }
 }
