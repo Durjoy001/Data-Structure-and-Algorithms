933da8d3ba844b9b257ad097cafdeeaea9b72fa2
Durjoy Dey <durjoydey001@gmail.com>
2020-08-26 21:41:13 +0600

    Update Detect Loop in linked list.cpp

--- Linked List/Singly Linked List/GeeksforGeeks solved problem/Floyd’s Cycle-Finding Algorithm/Detect Loop in linked list.cpp
+++ Linked List/Singly Linked List/GeeksforGeeks solved problem/Floyd’s Cycle-Finding Algorithm/Detect Loop in linked list.cpp
@@ -4,8 +4,8 @@ bool detectLoop(Node* head)
 {
     Node *slow_ptr = head;
     Node *fast_ptr = head;
-    while(slow_ptr && fast_ptr && fast_ptr->next)//increase fast_ptr by two node so there have to atlest one node after fast_ptr.
-    // If we one of this pointer points to NULL that's mean there are NO LOOP.
+    while(slow_ptr && fast_ptr && fast_ptr->next)//increase fast_ptr by two node so we have to check that atlest one node have after fast_ptr.
+    // If one of this pointer points to NULL that's mean there are NO LOOP.
     {
         slow_ptr = slow_ptr->next;
         fast_ptr = fast_ptr->next->next;
