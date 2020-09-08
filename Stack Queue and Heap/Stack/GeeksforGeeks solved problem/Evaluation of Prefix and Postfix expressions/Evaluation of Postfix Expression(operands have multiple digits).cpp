f7e95d045f0f7726f0dc07924cd341a2dc98a83b
Durjoy Dey <durjoydey001@gmail.com>
2020-09-08 19:36:20 +0600

    Update Evaluation of Postfix Expression(operands have multiple digits).cpp

--- Stack Queue and Heap/Stack/GeeksforGeeks solved problem/Evaluation of Prefix and Postfix expressions/Evaluation of Postfix Expression(operands have multiple digits).cpp
+++ Stack Queue and Heap/Stack/GeeksforGeeks solved problem/Evaluation of Prefix and Postfix expressions/Evaluation of Postfix Expression(operands have multiple digits).cpp
@@ -38,9 +38,9 @@ void evaluatePostFix(string s)
     {
         if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/')
         {
-            e2 = stk.top();
+            e2 = stk.top();//2nd element,cz 2nd element pop out in first
             stk.pop();
-            e1 = stk.top();
+            e1 = stk.top();//1st element,cz 1st element pop out in second
             stk.pop();
             x = operation(e1,e2,s[i]);
             stk.push(x);
