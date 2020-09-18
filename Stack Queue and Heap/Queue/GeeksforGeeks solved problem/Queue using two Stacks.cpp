9584514b57292a37267d77cc0644e5c7deb55039
Durjoy Dey <durjoydey001@gmail.com>
2020-09-18 20:11:57 +0600

    Update Queue using two Stacks.cpp

---
+++ Stack Queue and Heap/Queue/GeeksforGeeks solved problem/Queue using two Stacks.cpp
@@ -0,0 +1,29 @@
+
+
+/* The method push to push element into the queue */
+void StackQueue :: push(int x)
+ {
+        s1.push(x);
+ }
+
+/*The method pop which return the element poped out of the queue*/
+int StackQueue :: pop()
+{
+    if(s1.empty())
+    {
+        return -1;
+    }
+    while(!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    int Z = s2.top();
+    s2.pop();
+    while(!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+    return Z;
+}
