e53d867d1ea3422bad8c63b0cd25357504ea00e2
Durjoy Dey <durjoydey001@gmail.com>
2020-09-30 19:40:25 +0600

    Update Find the Factorial of n using recursion.cpp

--- Recursion and Backtracking/Recursion/Find the Factorial of n using recursion.cpp
+++ Recursion and Backtracking/Recursion/Find the Factorial of n using recursion.cpp
@@ -1,1 +1,20 @@
+//https://www.geeksforgeeks.org/recursion/
+//https://www.youtube.com/watch?v=_OmRGjbyzno&list=PL2_aWCzGMAwLz3g66WrxFGSXvSsvyfzCO
 
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n)
+{
+    //base condition
+    if(n==0)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+}
