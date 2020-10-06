49ac1e4dcc4583c47e1da34c7eeccf5645a98468
Durjoy001 <durjoydey001@gmail.com>
2020-10-06 19:00:37 +0600

    Update Level Order Traversal.cpp

--- Tree/Binary Tree/Binary tree traversal/Level Order Traversal.cpp
+++ Tree/Binary Tree/Binary tree traversal/Level Order Traversal.cpp
@@ -1,65 +1,24 @@
-//https://www.youtube.com/watch?v=86g8jAQug04&list=PL-pUjcDnciX3Z5AEE8HHRrcfj-987Ia94&index=9
-//https://www.geeksforgeeks.org/level-order-tree-traversal/
+//https://practice.geeksforgeeks.org/problems/level-order-traversal/1
 
-#include<bits/stdc++.h>
-using namespace std;
-class Node{
-public:
-    int data;
-    Node* left;
-    Node* right;
-};
-// Function to print Nodes in a binary tree in Level order
-void LevelOrder(Node* root)
+vector<int> levelOrder(Node* node)
 {
-    if(root == NULL)
-    {
-        return;
-    }
-    queue<Node*> Q;//for storing all Node address
-    Q.push(root);//push root node address
-    //while there is at least one unvisited node
-    while(!Q.empty())
-    {
-        Node* current = Q.front();
-        Q.pop();//removing the element at front
-        char x = current->data;
-        cout<<x<<" ";
-        if(current->left != NULL)
-        {
-            Q.push(current->left);
-        }
-        if(current->right != NULL)
-        {
-            Q.push(current->right);
-        }
-    }
+  //Your code here
+  vector<int>ans;
+  queue<Node*>Q;
+  Q.push(node);
+  while(!Q.empty())
+  {
+      Node* tmp = Q.front();
+      Q.pop();
+      ans.push_back(tmp->data);
+      if(tmp->left != NULL)
+      {
+          Q.push(tmp->left);
+      }
+      if(tmp->right != NULL)
+      {
+          Q.push(tmp->right);
+      }
+  }
+  return ans;
 }
-Node* Insert(Node* root,char data)
-{
-    if(root==NULL)
-    {
-        root = new Node();
-        root->data = data;
-        root->left = NULL;
-        root->right = NULL;
-    }
-    else if(data <= root->data)
-    {
-        root->left = Insert(root->left,data);
-    }
-    else
-    {
-        root->right = Insert(root->right,data);
-    }
-    return root;
-}
-int main()
-{
-    Node* root = NULL;
-    root = Insert(root,'M'); root = Insert(root,'B');
-	root = Insert(root,'Q'); root = Insert(root,'Z');
-	root = Insert(root,'A'); root = Insert(root,'C');
-	LevelOrder(root);
-}
-
