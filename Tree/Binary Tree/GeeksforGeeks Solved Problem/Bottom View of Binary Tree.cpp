c7717bbcd0bc1b7e853f3f8e2c6e3130f9b1e084
Durjoy Dey <durjoydey001@gmail.com>
2020-10-10 11:56:07 +0600

    Update Bottom View of Binary Tree.cpp

--- Tree/Binary Tree/GeeksforGeeks Solved Problem/Bottom View of Binary Tree.cpp
+++ Tree/Binary Tree/GeeksforGeeks Solved Problem/Bottom View of Binary Tree.cpp
@@ -1,6 +1,6 @@
 //https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
 
-//If don't understand than see vertical travesal code
+//If don't understand then see vertical travesal code
 // Method that returns the bottom view.
 vector <int> bottomView(Node *root)
 {
