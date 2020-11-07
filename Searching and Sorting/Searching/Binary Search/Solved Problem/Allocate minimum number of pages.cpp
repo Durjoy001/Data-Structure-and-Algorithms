76e049cc74c769c95b4d41fa5964406dec5fc989
Durjoy Dey <durjoydey001@gmail.com>
2020-11-07 17:56:41 +0600

    Create Allocate minimum number of pages.cpp

---
+++ Searching and Sorting/Searching/Binary Search/Solved Problem/Allocate minimum number of pages.cpp
@@ -0,0 +1,68 @@
+//https://www.geeksforgeeks.org/allocate-minimum-number-pages/
+//https://www.youtube.com/watch?v=2JSQIhPcHQg
+
+bool isValid(int *arr,int n,int m,int mid)
+{
+    int student = 1;
+    int sum = 0;
+    for(int i=0;i<n;i++)
+    {
+        // check if current number of pages are greater 
+        // than curr_min that means we will get the result 
+        // after mid no. of pages 
+        if(arr[i]>mid)
+        {
+            return false;
+        }
+        sum = sum + arr[i];
+        if(sum > mid)
+        {
+            student++;
+            sum = arr[i];
+        }
+        if(student > m)
+        {
+            return false;
+        }
+    }
+    //there maybe a case when student would be less than m;
+    //but we have to return true,cz it is possible to divide this much of page 
+    //or less than this to every students.
+    //logic is if less number of student can read this much of page
+    //then it obious that more number of students also can read this much
+    //or less than this much of page.
+    return true;
+}
+int findPages(int *arr, int n, int m) {
+    int sum = 0;
+    int MX = 0;
+    //array maybe sorted or unsorted but we can't sort the array 
+    //if array is unsorted then also we can use this binary search stratagy
+    for(int i=0;i<n;i++)
+    {
+        sum = sum + arr[i];
+        MX = max(arr[i],MX);
+    }
+    if(m > n)//student is more than number of books
+    {
+        return -1;
+    }
+    int low = MX;//minimum possible value
+    int high = sum;//maximum possible value
+    int mid;
+    int result = -1;
+    while(low <= high)
+    {
+        mid = low + (high - low)/2;
+        if(isValid(arr,n,m,mid) == true)
+        {
+            result = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    return result;
+}
