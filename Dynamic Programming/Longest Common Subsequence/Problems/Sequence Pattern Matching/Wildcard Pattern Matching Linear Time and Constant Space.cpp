442acfc7447862fda409fec48f439558bf5345e7
Durjoy Dey <durjoydey001@gmail.com>
2021-03-22 23:44:19 +0600

    Create Wildcard Pattern Matching Linear Time and Constant Space.cpp

---
+++ Dynamic Programming/Longest Common Subsequence/Problems/Sequence Pattern Matching/Wildcard Pattern Matching Linear Time and Constant Space.cpp
@@ -0,0 +1,38 @@
+
+
+class Solution{
+  public:
+/*You are required to complete this method*/
+    int wildCard(string pattern,string str)
+    {
+        int m = pattern.size();
+        int n = str.size();
+        int i=0,j=0,index_str=-1,index_pat=-1;
+        while(i < n){
+            if(j < m && ((str[i] == pattern[j]) || pattern[j] == '?'))
+            {
+                i++,j++;
+            }
+            else if(j < m && pattern[j] == '*'){
+                index_str = i;
+                index_pat = j;
+                j++;
+            }
+            else if(index_pat != -1){
+                i = index_str+1;
+                j = index_pat+1;
+                index_str++;
+            }
+            else{
+                return false;
+            }
+        }
+        while(j < m && pattern[j] == '*'){
+            j++;
+        }
+        if(j == m){
+            return 1;
+        }
+        return 0;
+    }
+};
